9ca4e7cf474a9bc0a284e42c8effedf2b8d8b448
timothy <r10922188@csie.ntu.edu.tw>
2023-10-02 10:19:14 +0800

    fix: database init will now check connections

--- database/database.go
+++ database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -11,8 +12,14 @@ var client *mongo.Client
 
 func Init(url string) error {
 	serverApi := options.ServerAPI(options.ServerAPIVersion1)
-	clientOptions := options.Client().ApplyURI(url).SetServerAPIOptions(serverApi)
-	database, err := mongo.Connect(context.TODO(), clientOptions)
+	clientOptions := options.Client().ApplyURI(url).SetServerAPIOptions(serverApi).SetConnectTimeout(10)
+	database, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		return err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	err = database.Ping(ctx, nil)
 	client = database
 	return err
 }
